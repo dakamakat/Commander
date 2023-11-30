23cb3a3d8dd12933e67bfeea67fb91ac55adbf0d
dakamakat <gazon-ozon@yandex.ru>
2023-11-30 14:06:31 +0100

    feat(clap/basic): add basic proof of concept / clap toml config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+use std::process::Command;
+use std::str;
+
+mod models;
+
 fn main() {
-    println!("Hello, world!");
+    let args = models::command::ActionUnit::parse();
+
+    let output = Command::new(args.body)
+        .current_dir(args.path)
+        .output()
+        .expect("Failed to execute command");
+
+    let mut log = String::new();
+
+    log.push_str(match str::from_utf8(&output.stdout) {
+        Ok(val) => val,
+        Err(_) => panic!("got non UTF-8 data from git"),
+    });
+
+    println!("{0}", log)
 }
