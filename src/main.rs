c1cb1adbab1638f14dfa0a3da33188d06d4dbc07
dakamakat <gazon-ozon@yandex.ru>
2023-12-07 10:05:45 +0100

    feat(basic): add basic invokation for actions

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
-use clap::Parser;
 use std::process::Command;
-use std::str;
+
+use clap::Parser;
 
 mod models;
+mod parser;
+mod store;
 
 fn main() {
-    let args = models::command::ActionUnit::parse();
+    let val = models::command::ActionUnit::parse();
 
-    let output = Command::new(args.body)
-        .current_dir(args.path)
-        .output()
-        .expect("Failed to execute command");
+    match val.action_type {
+        models::command::ActionType::File(val) => match val.command {
+            models::command::FileSubcommand::Create(_val) => todo!(),
+            models::command::FileSubcommand::Delete => todo!(),
+            models::command::FileSubcommand::Show => todo!(),
+        },
+        models::command::ActionType::Action(val) => match val.command {
+            models::command::ActionSubcommand::Invoke(val) => {
+                println!("body: {0} , path: {1} \n", val.body, val.path);
 
-    let mut log = String::new();
+                let output = Command::new(val.body)
+                    .current_dir(val.path)
+                    .output()
+                    .expect("Failed to execute command");
 
-    log.push_str(match str::from_utf8(&output.stdout) {
-        Ok(val) => val,
-        Err(_) => panic!("got non UTF-8 data"),
-    });
+                let mut log = String::new();
 
-    println!("{0}", log)
+                log.push_str(match std::str::from_utf8(&output.stdout) {
+                    Ok(val) => val,
+                    Err(_) => panic!("got non UTF-8 data from git"),
+                });
+
+                println!("command output:\n{0}", log,)
+            }
+        },
+    }
 }
