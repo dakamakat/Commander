db492a84ed092c214c209b306344a0d027f31b87
dakamakat <gazon-ozon@yandex.ru>
2023-12-12 09:30:12 +0100

    refactor(main): update commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+mod actions;
 mod models;
 mod parser;
 mod store;
 
+use actions::invoke::invoke;
 use anyhow::Result;
 use clap::Parser;
 use models::command::{ActionSubcommand, ActionType, FileSubcommand};
-use std::process::Command;
 use store::filesystem::{create_command_file, delete_command_file, read_command_file};
 
 fn main() -> Result<()> {
@@ -18,23 +19,7 @@ fn main() -> Result<()> {
             FileSubcommand::Read(val) => return read_command_file(val),
         },
         ActionType::Action(val) => match val.command {
-            ActionSubcommand::Invoke(val) => {
-                println!("body: {0} , path: {1} \n", val.body, val.path);
-
-                let output = Command::new(val.body)
-                    .current_dir(val.path)
-                    .output()
-                    .expect("Failed to execute command");
-
-                let mut log = String::new();
-
-                log.push_str(match std::str::from_utf8(&output.stdout) {
-                    Ok(val) => val,
-                    Err(_) => panic!("got non UTF-8 data from git"),
-                });
-
-                println!("command output:\n{0}", log,)
-            }
+            ActionSubcommand::Invoke(val) => invoke(val.body, val.path),
         },
     }
     Ok(())
